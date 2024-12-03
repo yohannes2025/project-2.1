2a7ac095573dc4d072e80f63a798348380aa13e0
Yohannes Mebrahtu <johannes.m.tekle@gmail.com>
2024-12-03 10:43:06 +0000

    add basic functionality

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,41 @@ document.addEventListener("DOMContentLoaded", function(){
     const display = document.getElementById('calc-display');
     const buttons = document.getElementsByClassName('btn');
 
+    let currentValue = "";
+
+    function evaluateResult(){
+        console.log('current value: ', currentValue);
+        const convertedValue = currentValue
+            .replace("×", "*")
+            .replace("÷", "/")
+            .replace("%", "*0.01")
+        console.log('converted value: ', convertedValue);
+        const result = eval(convertedValue);
+        currentValue = result.toString();
+        display.value = currentValue;
+    }
+
     for(let i = 0; i < buttons.length; i++){
         const button = buttons[i];
         button.addEventListener('click', function(){
-            console.log('button clicked:', button.innerText);
+            const value = button.innerText;
+
+            if(value == "AC"){
+                currentValue = "";
+                display.value = currentValue;
+            }else if(value == "="){
+                evaluateResult();
+            } 
+                
+                
+               else {
+                currentValue += value;
+                display.value = currentValue;
+                //console.log('currentValue:', currentValue);
+                }
+
+            
+            
         })
         
     }    
